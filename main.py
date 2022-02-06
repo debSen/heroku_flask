06f91fca4f9c55ac06d8fdde95bd9864dd55c9f8
Debapriya Sen <desen@akamai.com>
2022-02-06 18:56:08 +0530

    added base logic to home

--- main.py
+++ main.py
@@ -3,15 +3,25 @@ import os
 
 app = Flask(__name__)
 
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'favicon.ico', mimetype='image/favicon.png')
+posts = [
+    {
+        'author':'Deb',
+        'title': 'post 1',
+        'content': 'my first blog post',
+        'date_posted': 'February 6th, 2022'
+    },
+    {
+        'author':'Deb',
+        'title': 'post 2',
+        'content': 'my second blog post',
+        'date_posted': 'February 7th, 2022'
+    },   
+]
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 if __name__ == "__main__":
     app.secret_key = 'ItIsASecret'
